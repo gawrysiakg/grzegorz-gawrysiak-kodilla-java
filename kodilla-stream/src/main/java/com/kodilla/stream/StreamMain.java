61155451e380d23a856702987139303ecb1fe345
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2022-08-17 23:18:50 +0200

    7.2 Immutable ForumUser class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,8 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.immutable.ForumUser;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -15,7 +17,6 @@ public class StreamMain {
         expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
         System.out.println("Calculating expressions with method references");
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
@@ -23,6 +24,7 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
 
+    //  7.1 Exercise:
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         System.out.println(poemBeautifier.beautify("This is a example text created with  expression", String::toUpperCase));
@@ -30,6 +32,13 @@ public class StreamMain {
         System.out.println(poemBeautifier.beautify("This is a example text created with lambda expression", text->text.toLowerCase().concat(" ->")));
         System.out.println(poemBeautifier.beautify("This is a example text created with lambda expression ", text->text.toUpperCase().repeat(2).replace(' ', '_')));
         System.out.println(poemBeautifier.beautify("This is a example text created with lambda expression", text->text.replace("text", "EXTRA TEXT")));
+
+
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+
 
     }
 }
