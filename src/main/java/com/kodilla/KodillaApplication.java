9f8ce39ced49b2e9a4e61a8c5f3af53e89e1a8d2
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2022-12-15 19:33:20 +0100

    build gradle refactoring

--- src/main/java/com/kodilla/KodillaApplication.java
+++ src/main/java/com/kodilla/KodillaApplication.java
@@ -1,12 +1,12 @@
 package com.kodilla;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class KodillaApplication {
 
     public static void main(String[] args) {
-       // SpringApplication.run(KodillaApplication.class, args);
+        SpringApplication.run(KodillaApplication.class, args);
     }
 }
--- src/test/java/com/kodilla/KodillaTestApplication.java
+++ src/test/java/com/kodilla/KodillaTestApplication.java
@@ -1,4 +1,12 @@
 package com.kodilla;
 
+import org.junit.jupiter.api.Test;
+
 public class KodillaTestApplication {
+
+    @Test
+    public void contextLoad() {
+
+
+    }
 }
